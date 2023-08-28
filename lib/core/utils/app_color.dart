aa28b8ed6f4cbbcc83b4a9052b400c9c55f2ecd8
namankashyap <naman.k@purplle.com>
2023-08-28 10:23:42 +0530

    - Utils added to get random color - App color added - List view changed to grid view - App bar changed - google_fonts added

---
+++ lib/core/utils/app_color.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppColor{
+  static const boxColor1=Color(0xFFD9E8FC);
+  static const boxColor2=Color(0xFFFFD8F4);
+  static const boxColor3=Color(0xFFFDE99D);
+  static const boxColor4=Color(0xFFB0E9CA);
+  static const boxColor5=Color(0xFFFFEADD);
+  static const boxColor6=Color(0xFFFCFAD9);
+  static const searchBgColor=Color(0xFFF0F0F0);
+
+}
